2234da9fb3599ae9373062d91f13baf475f7d46c
Zsolt Andorko <zsolt.andorko.111@gmail.com>
2026-04-04 17:20:13 +0200

    feat(frontend/auth): add token interceptor and fix logout reactivity

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,6 +1,22 @@
 <script>
+import { http } from "@/utils/http.js";
 export default {
   name: 'App',
+  async created() {
+    const token = localStorage.getItem('token')
+    if (token) {
+      try {
+        const resp = await http.get('/api/auth/check')
+
+        this.$root.user = resp.data.user
+
+        localStorage.setItem('abilities', JSON.stringify(resp.data.abilities))
+      } catch {
+        localStorage.removeItem('token')
+        localStorage.removeItem('abilities')
+      }
+    }
+  }
 }
 </script>
 
